3eb2fab7b186cc4ece9d910ef59ed324fc861abf
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2023-04-08 16:27:43 +0200

    All components complete

--- lib/portfolio.ts
+++ lib/portfolio.ts
@@ -6,11 +6,11 @@ type Portfolio = {
 export const portfolio:Portfolio={
   portfolio:'PORTFOLIO',
   description:'The Lists show some landing pages and components created for fun. And it will interest me if you have a look and feel of them.',
-  projects:[{name:'Website 1',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/public/site--1.png'},
-  {name:'Component 1',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/public/comp--1.png'},
-  {name:'Component 2',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/public/comp--2.png'},
-  {name:'Component 3',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/public/comp--3.png'},
-  {name:'Component 4',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/public/comp--4.png'},
-  {name:'LandingPage 1',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/public/land--1.png'},
-  {name:'LandingPage 2',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/public/land--2.png'},]
+  projects:[{name:'Website 1',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/../public/site--1.png'},
+  {name:'Component 1',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/../public/comp--1.png'},
+  {name:'Component 2',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/../public/comp--2.png'},
+  {name:'Component 3',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/../public/comp--3.png'},
+  {name:'Component 4',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/../public/comp--4.png'},
+  {name:'LandingPage 1',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/../public/land--1.png'},
+  {name:'LandingPage 2',link:'https://portfolio-git-master-cen-smart.vercel.app/',path:'/../public/land--2.png'},]
 }
